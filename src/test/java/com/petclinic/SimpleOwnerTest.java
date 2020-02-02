c8eb39f169452630831ca9b6b4e070bbda6a0a5c
Alexey Panashchenko <alpa@ciklum.com>
2020-02-02 11:18:06 +0200

    Add Simple Owner test suite

---
+++ src/test/java/com/petclinic/SimpleOwnerTest.java
@@ -0,0 +1,29 @@
+package com.petclinic;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+/**
+ * Created by alpa on 1/30/20
+ */
+public class SimpleOwnerTest {
+
+    @BeforeClass
+    public void setUp() {
+        Configuration.baseUrl = "http://139.59.149.247:8000/petclinic";
+    }
+
+
+    @Test
+    public void addButtonShouldBeTest() {
+        open("/owners/add");
+        NewOwnerPage newOwnerPage = new NewOwnerPage();
+
+        newOwnerPage.addButtonShouldBe(Condition.disabled);
+    }
+}
